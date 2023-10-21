016faa1928baed2330bbc2decc7a32ab1e0b243f
amir <shahidi.amir78@gmail.com>
2023-10-21 19:16:40 +0330

    Paginate issues

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -30,7 +30,7 @@ const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
   };
 
   return (
-    <Flex align={"center"} gap={"3"}>
+    <Flex mt="4" align={"center"} gap={"3"}>
       <Text size={"2"}>
         Page {currentPage} of {pageCount}
       </Text>
